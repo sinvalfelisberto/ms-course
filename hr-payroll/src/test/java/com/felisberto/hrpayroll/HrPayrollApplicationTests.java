da38f04b7b734f8a1f3aedb352eef3e806114989
sinvalfelisberto <sinvalfelisberto@gmail.com>
2025-01-01 11:49:14 -0300

    adicionando projeto payroll

---
+++ hr-payroll/src/test/java/com/felisberto/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felisberto.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
