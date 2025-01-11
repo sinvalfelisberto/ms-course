34cd175281d9f464e62f0498f08c0777b60401a7
sinvalfelisberto <sinvalfelisberto@gmail.com>
2025-01-11 07:03:36 -0300

    database seeding

--- hr-user/src/main/java/com/felisberto/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/felisberto/hruser/HrUserApplication.java
@@ -1,15 +1,26 @@
 package com.felisberto.hruser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner {
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		//script pra gerar o hash de senhas pra salvar no db
+		//System.out.println("BCRYPT= " + bCryptPasswordEncoder.encode("123456"));
+	}
 }
